8bf04f946b8fce47e2b887914afb6cc455c9a9b7
Wilson Neto B.R <contato@wilsonneto.com.br>
2020-08-08 22:11:49 -0300

    Update HomeController.cs

--- SiteContagem/Controllers/HomeController.cs
+++ SiteContagem/Controllers/HomeController.cs
@@ -28,7 +28,7 @@ namespace SiteContagem.Controllers
                 TempData["Local"] = _CONTADOR.Local;
                 TempData["Kernel"] = _CONTADOR.Kernel;
                 TempData["TargetFramework"] = _CONTADOR.TargetFramework;
-                TempData["MensagemFixa"] = "Teste";
+                TempData["MensagemFixa"] = "Teste Rolou!!";
                 TempData["MensagemVariavel"] = configuration["MensagemVariavel"];
             }            
 
